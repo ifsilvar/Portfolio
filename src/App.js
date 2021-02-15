4d2d32f4191a32eae1194463a0378a4bc9e23246
Ivan Flores <ifsilvar4@gmail.com>
2021-02-15 00:58:39 -0800

    installed tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home.js"
+import NavBar from "./components/NavBar.js"
+import "tailwindcss/tailwind.css"
 
 function App() {
   return (
     <BrowserRouter>
+    <NavBar />
       <Switch>
         <Route component={Home} path="/"></Route>
       </Switch>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,11 +3,11 @@ import { NavLink } from "react-router-dom"
 
 export default function NavBar() {
     return (
-    <header>
-        <div>
+    <header className="bg-blue-500">
+        <div className="container mx-auto flex justify-between">
             <nav>
-                <NavLink>
-                    
+                <NavLink to="/">
+                Home Link
                 </NavLink>
             </nav>
         </div>
