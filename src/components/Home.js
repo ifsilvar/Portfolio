6d5d85d16a0bbc8581dcee33de0a42d0fd6cb70a
Ivan Flores <ifsilvar4@gmail.com>
2021-02-15 01:22:47 -0800

    initial styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,13 @@
 import React from "react";
+import image from "../background.png"
 
 export default function Home() {
-    return <h1>Home Page!</h1>
+    return (
+    <main>
+        <img src={image} alt="homescreen background" className="absolute object-cover w-full h-full" />
+        <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+            <h1 className="text-6xl text-green-300 font-bold leading-none lg:leading-snug">Hello Im Ivan</h1>
+        </section>
+    </main>
+        )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { NavLink } from "react-router-dom"
+import { SocialIcon } from "react-social-icons"
 
 export default function NavBar() {
     return (
     <header className="bg-blue-500">
         <div className="container mx-auto flex justify-between">
-            <nav>
-                <NavLink to="/">
+            <nav className="flex">
+                <NavLink to="/" exact className="inline-flex items-center py-3 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold tracking-widest">
                 Home Link
                 </NavLink>
             </nav>
+            <div className="inline-flex py-3 px-3 ">
+                <SocialIcon url="https://www.linkedin.com/in/ivan-f-silvar/" className="mr-4" target="_blank" fgColor="fff" style={{ height: 35, width: 35 }} />
+            </div>
         </div>
     </header>
     )
