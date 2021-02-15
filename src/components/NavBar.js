38c5e633977540008b6e47111098d9e26eef0a29
Ivan Flores <ifsilvar4@gmail.com>
2021-02-14 23:41:03 -0800

    fixed package json lock

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { NavLink } from "react-router-dom"
+
+export default function NavBar() {
+    return (
+    <header>
+        <div>
+            <nav>
+                <NavLink>
+                    
+                </NavLink>
+            </nav>
+        </div>
+    </header>
+    )
+}
