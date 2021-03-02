540393fe49d7da3f95d74119798f7c03dfb665d5
Ivan Flores <ifsilvar4@gmail.com>
2021-03-02 02:12:11 -0800

    removed placeholder text in contact card

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,8 +6,8 @@ export default function Contact() {
             <div className="container mx-auto min-h-scree*">
                 <h1 className="text-bold text-4xl text-center text-navy">Contact</h1>
                 <div class="sm:w-5/6 lg:w-1/2 md:w-2/3 bg-white rounded-3xl p-8 flex flex-col mx-auto mt-10 shadow-md">
-                    <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
-                    <p class="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
+                    <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Lets Connect!</h2>
+                    <p class="leading-relaxed mb-5 text-gray-600">Communication is 🔑. Contact me for jobs, feedback, or just to say "Hi!"</p>
                     <div class="relative mb-4">
                         <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
                         <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
@@ -17,7 +17,7 @@ export default function Contact() {
                         <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                     </div>
                     <button class="text-white bg-linkedin-blue border-0 py-2 px-6 focus:outline-none hover:bg-navy rounded text-lg">Send</button>
-                    <p class="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
+                    <p class="text-xs text-gray-500 mt-3">Thank you for your time.</p>
                 </div>
             </div>
         </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,13 +8,13 @@ export default function Footer() {
         <footer className="text-gray-600 body-font">
             <div className="container px-5 py-8 flex justify-center items-center mx-auto">
                 <a href="https://drive.google.com/file/d/1AHJzlgisu4rUuqo2CIeY4ZfZ6kvmmhGT/view?usp=sharing" className="text-gray-900">
-                    <FontAwesomeIcon className="text-gray-100 mx-2 md:mx-5" target="_blank" rel="noreferrer"  style={{ height: 50, width: 50 }} icon={faFilePdf} />
+                    <FontAwesomeIcon className="text-gray-100 mx-5" target="_blank" rel="noreferrer"  style={{ height: 50, width: 50 }} icon={faFilePdf} />
                 </a>
                 <a href="https://github.com/ifsilvar" className=" text-gray-900">
-                    <FontAwesomeIcon className="text-gray-100 mx-2 md:mx-5" target="_blank" rel="noreferrer" style={{ height: 50, width: 50 }} icon={faGithub} />
+                    <FontAwesomeIcon className="text-gray-100 mx-5" target="_blank" rel="noreferrer" style={{ height: 50, width: 50 }} icon={faGithub} />
                 </a>
                 <a href="https://www.linkedin.com/in/ivan-f-silvar/" className="text-gray-900">
-                    <FontAwesomeIcon className="text-gray-100 mx-2 md:mx-5" target="_blank" rel="noreferrer" style={{ height: 50, width: 50 }} icon={faLinkedin} />
+                    <FontAwesomeIcon className="text-gray-100 mx-5" target="_blank" rel="noreferrer" style={{ height: 50, width: 50 }} icon={faLinkedin} />
                 </a>
             </div>
         </footer>
