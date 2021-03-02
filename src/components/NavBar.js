313625657e784c975ff283ed9c57c9be394d9e4c
Ivan Flores <ifsilvar4@gmail.com>
2021-03-02 02:02:09 -0800

    added small screen text size

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,9 +10,9 @@ export default function NavBar() {
                 {/* <p className="inline-flex items-center py-3 px-3 text-gray-100 hover:text-green-800 text-3xl font-bold tracking-widest">
                 Ivan
                 </p> */}
-                <a href="#about" className="inline-flex items-center mb-1 py-3 px-3 text-gray-100 hover:text-linkedin-blue text-2xl font-bold tracking-widest">About</a>
-                <a href="#projects" className="inline-flex items-center mb-1 py-3 px-3 text-gray-100 hover:text-linkedin-blue text-2xl font-bold tracking-widest">Projects</a>
-                <a href="#contact" className="inline-flex items-center mb-1 py-3 px-3 text-gray-100 hover:text-linkedin-blue text-2xl font-bold tracking-widest">Contact</a>
+                <a href="#about" className="inline-flex items-center mb-1 py-3 px-3 text-gray-100 hover:text-linkedin-blue md:text-2xl font-bold tracking-widest">About</a>
+                <a href="#projects" className="inline-flex items-center mb-1 py-3 px-3 text-gray-100 hover:text-linkedin-blue md:text-2xl font-bold tracking-widest">Projects</a>
+                <a href="#contact" className="inline-flex items-center mb-1 py-3 px-3 text-gray-100 hover:text-linkedin-blue md:text-2xl font-bold tracking-widest">Contact</a>
             </nav>
             <div className="inline-flex items-center justify-center shadow-md rounded-full mt-2 mb-2 pr-3">
                 <SocialIcon url="https://www.linkedin.com/in/ivan-f-silvar/" className="" target="_blank" fgColor="fff" style={{ height: 50, width: 50 }} />
